d2902aeae325bc12ac9dab374480a2416fb6ebf8
tim_jackins <jack@jacktimmins.com>
2020-08-29 13:10:11 -0400

    Initial Commit - Build off Djangox - Setup python environment for VSC

---
+++ config/urls.py
@@ -0,0 +1,21 @@
+from django.conf import settings
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    # Paths for website
+    path('admin/', admin.site.urls),
+    path('users/', include('django.contrib.auth.urls')),
+    path('accounts/', include('allauth.urls')),
+    path('', include('pages.urls')),
+
+    # Paths for API
+    path('api/mascots/', include('mascots.urls')),
+    path('api/teams/', include('teams.urls'))
+]
+
+if settings.DEBUG:
+  import debug_toolbar
+  urlpatterns = [
+      path('__debug__/', include(debug_toolbar.urls)),
+  ] + urlpatterns
